85118a26724e504c576cb6dbd221aecf0d5e0d1a
Kennedy1-svg <kennyohia@gmail.com>
2026-04-09 00:01:20 +0100

    added number

--- src/app.js
+++ src/app.js
@@ -87,7 +87,7 @@ const anthropic = new Anthropic({
 const octokit = new Octokit({
   authStrategy: createAppAuth,
   auth: {
-    appId: process.env.APP_ID,
+    appId: Number(process.env.APP_ID),
     privateKey: process.env.PRIVATE_KEY,
     installationId: process.env.INSTALLATION_ID,
   },
